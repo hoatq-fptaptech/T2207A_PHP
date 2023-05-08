3c17566307b4e4b3309ef28af618ab477319c4ee
macbook <quanghoa.trinh@gmail.com>
2023-05-08 10:03:50 +0700

    login

---
+++ post-login.php
@@ -0,0 +1,28 @@
+<?php
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$host = "localhost";
+$user = "root";
+$pwd = "root";
+$db = "t2207a";
+
+$conn = new mysqli($host,$user,$pwd,$db);
+if($conn->connect_error){
+    die("Connect error...");
+}
+$sql_check = "select * from users where email like '$email'";
+$check = $conn->query($sql_check);
+if($check->num_rows == 0){
+    die("Email hoặc mật khẩu không chính xác");
+}
+$user = null;
+while ($row = $check->fetch_assoc()){
+    $user = $row;
+}
+$compare = password_verify($password,$user["passowrd"]);
+if($compare){
+    die("Đăng nhập thành công");
+}else{
+    die("Email hoặc mật khẩu không chính xác");
+}
